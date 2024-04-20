9eb6607bdc5a4eec588a354eff68bd7102dbdc6f
Caua <cauacampos05@gmail.com>
2024-04-20 14:40:14 -0300

    feat(tab2page): "atendente" page created

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -10,11 +10,17 @@ export class SenhasService {
   public senhasExame: number = 0;
   public senhasTotal: number = 0;
   public inputNovaSenha: string ='';
-  private senhasArray: { [key: string]: string[] } = { // Use string[] for efficiency
+  private senhasArray: { [key: string]: string[] } = {
     SG: [],
     SP: [],
     SE: []
   };
+  public senhasAtendidas: {[key:string]: string[]} = {
+    SG: [],
+    SP: [],
+    SE: []
+  };
+  public senhasAtendidasQtd: number = 0;
 
 
   constructor() {}
@@ -32,6 +38,16 @@ export class SenhasService {
   somaExame() {
     this.senhasExame++;
     this.senhasTotal++;
+  }
+
+  tempoDeAtendimento(yx: number, variancia: number): number {
+    const standardDeviation = Math.sqrt(variancia);
+    let sum = 0;
+    for (let i = 0; i < 12; i++) {
+        sum += Math.random();
+    }
+    const numero = (sum - 6) * standardDeviation + yx;
+    return numero;
   }
 
   novaSenha(tipoSenha: string = '') {
@@ -66,7 +82,37 @@ export class SenhasService {
         (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
         this.senhasArray['SE'].push(this.inputNovaSenha);
     }
-
-    console.log(this.senhasArray);
+    
+    // console.log(this.senhasArray);
+    console.log(this.senhasArray['SP'].slice(-5));
   }
+
+  chamarSenha(tipoSenha: String = '') {
+    if (this.senhasArray['SP'].length) {
+      let x = this.senhasArray['SP'].splice(0,1)[0];
+      this.senhasAtendidas['SP'].push(x);
+      let timeP = this.tempoDeAtendimento(10,20);
+    } else if (this.senhasArray['SG'].length) {
+      let x = this.senhasArray['SG'].splice(0,1)[0];
+      this.senhasAtendidas['SG'].push(x);
+      let timeP = this.tempoDeAtendimento(1, 3);
+    } else if (this.senhasArray['SE'].length) {
+      let x = this.senhasArray['SE'].splice(0,1)[0];
+      this.senhasAtendidas['SE'].push(x);
+      let timeP = 1;
+    }
+    console.log(this.senhasAtendidas['SP'])
+    this.senhasAtendidasQtd++
+  }
+
+
+
+  // deletarSenha() {
+  //   for (let i = 0; i < this.senhasArray['SP'].length; i++) {
+  //     const element = this.senhasArray[i];
+  //     if (element != this.senhasAtendidas['SP']) {
+  //       this.senhasArray['SP'].replace(element)
+  //     }
+  //   }
+  // }
 }
